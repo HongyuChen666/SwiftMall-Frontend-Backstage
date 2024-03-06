ec71bc0424b521267d9c788dc6e00922ad1f201e
HongyuChen666 <158306080+HongyuChen666@users.noreply.github.com>
2024-03-05 19:18:17 -0500

    Delete components directory

--- components/Avatar/hooks/useUserinfo.js
+++
@@ -1,7 +0,0 @@
-import { storeToRefs } from 'pinia'
-import { useAccount } from '@/pinia/modules/account'
-
-export const useUserinfo = () => {
-  const { userinfo } = storeToRefs(useAccount())
-  return { userinfo }
-}
